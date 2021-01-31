525799ccbdb1b8868daba3bd34469a932df42f81
CodeFuller <CodeFuller@gmail.com>
2021-01-31 13:36:01 +0300

    Add docker-compose.yml for IT (PublicApiService)

--- src/Services/PublicApiService/tests/PublicApiService.IntegrationTests/CustomWebApplicationFactory.cs
+++ src/Services/PublicApiService/tests/PublicApiService.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using GraphQL.Client.Abstractions;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 
 namespace PublicApiService.IntegrationTests
 {
 	public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
 	{
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			base.ConfigureWebHost(builder);
+
+			builder.ConfigureAppConfiguration(configBuilder =>
+			{
+				configBuilder
+					.AddInMemoryCollection(new[] { new KeyValuePair<string, string>("services:updatesServiceAddress", "http://localhost:8102/") });
+			});
+		}
+
 		public IGraphQLClient CreateGraphQLClient()
 		{
 			var httpClient = CreateClient();
